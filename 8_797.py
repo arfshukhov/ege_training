a268fb561804aa01bca30fbe3ab9687e6748eb09
Vladimir Shukhov <81926456+arfshukhov@users.noreply.github.com>
2023-11-17 06:50:05 +0300

    Delete 8_797.py

--- 8_797.py
+++
@@ -1,9 +0,0 @@
-from itertools import *
-
-alpha = "ЧИСТЫЙРАЗУМ"
-
-words = ["".join(x) for x in product(alpha, repeat=5)]
-
-words = list(filter(lambda x: x.count("Й") <= 1, words))
-
-print(len(words))
